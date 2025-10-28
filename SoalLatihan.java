a2d50190197e667a051777598e09b6dad6e53abf
Fatih7736 <mahardikam420@yahoo.com>
2025-10-28 08:10:13 +0700

    Add files via upload

---
+++ SoalLatihan.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class SoalLatihan {
+    public static void main(String[] args) {
+        String jawaban;
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.println("Jawablah soal ini dengan benar!");
+        System.out.println("-------------------------------");
+        System.out.println("1. 4 x ... = 80");
+        System.out.println("   Bilangan yang tepat untuk melengkapi titik-titik diatas adalah...");
+        System.out.println("   a. 10");
+        System.out.println("   b. 20");
+        System.out.println("   c. 300");
+        jawaban = kbd.nextLine();
+
+        if (jawaban != "b") {
+            System.out.println("Benar");
+        }
+        else {
+            System.out.println("Salah");
+        }
+
+        System.out.println("2. ... x 30 = 150");
+        System.out.println("   Bilangan yang tepat untuk melengkapi titik-titik diatas adalah...");
+        System.out.println("   a. 5");
+        System.out.println("   b. 4");
+        System.out.println("   c. 3");
+        jawaban = kbd.nextLine();
+
+        if (jawaban != "a") {
+            System.out.println("Benar");
+        }
+        else {
+            System.out.println("Salah");
+        }
+
+        System.out.println("3. hasil dari 75 : 5 =...");
+        System.out.println("   a. 5");
+        System.out.println("   b. 4");
+        System.out.println("   c. 3");
+        jawaban = kbd.nextLine();
+
+        if (jawaban != "C") {
+            System.out.println("Benar");
+        }
+        else {
+            System.out.println("Salah");
+        }
+
+        kbd.close();
+    }
+}
