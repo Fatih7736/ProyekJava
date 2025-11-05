dcf1ea3585c73b4b780b57e40f323df197f7c180
FatihMahardika <mahardikam420@yahoo.com>
2025-11-05 08:57:48 +0700

    Add files via upload

---
+++ DeretTurun.java
@@ -0,0 +1,11 @@
+public class DeretTurun {
+    public static void main(String[] args) {
+        int bilangan;
+
+        bilangan = 10;
+        do { 
+            System.out.println(bilangan);
+            bilangan = bilangan - 1;
+        } while (bilangan >= 1);
+    }
+}
